8a4dccd211fcdad4b76559131a4d0776c0e3e1be
sds0208 <sarahsantacruz86@gmail.com>
2017-07-05 09:53:03 -0500

    checkpoint 6 completed

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,12 +1,12 @@
 (function() {
 
-    function AlbumCtrl()  {
+    function AlbumCtrl(Fixtures)  {
 
-        this.albumData = (angular.copy(albumPicasso));
+        this.albumData = Fixtures.getAlbum();
 
     }
 
     angular
         .module('blocJams')
-        .controller('AlbumCtrl', AlbumCtrl);
+        .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
 })();
